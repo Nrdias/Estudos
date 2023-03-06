dbd411d2c9559eace6a72d89907a5af121d45e72
Nrdias <natan.nrdias@hotmail.com>
2023-03-06 16:02:11 -0300

    New exercices of Collections using map, set e arraylists

--- src/est/dio/streamAPI/ExerciceMapList.java
+++ src/est/dio/streamAPI/ExerciceMapList.java
@@ -1,2 +1,12 @@
-package est.dio.streamAPI;public class ExerciceMapList {
+package est.dio.streamAPI;
+
+/*
+ * Crie uma lista com as seguintes informações
+ * id = 1 - contato = nome - number = 1111
+ * id = 2 - contato = nome - number = 2222
+ * id = 3 - contato = nome - number = 3333
+ * id = 4 - contato = nome - number = 4444
+ */
+
+public class ExerciceMapList {
 }
